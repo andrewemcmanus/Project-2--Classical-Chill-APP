194127eabc4daedad1404aec2481135082b557c8
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-08-13 17:01:18 -0700

    add flash

--- config/ppConfig.js
+++ config/ppConfig.js
@@ -1,34 +1,36 @@
-const passport = requir('passpot');
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
 const db = require('../models');
-const user = require('../models/user');
-const { combineTableNames } = require('sequelize/types/lib/utils');
-/* passport "serialize" your info make it easier on login */
-//-convert the user based on the id
-passport.serializeUser((user, cd)=>{
-    combineTableNames(null, user.id);
+/* Passport "serialize" your info make it easier to login
+  - Convert the user based on the id
+*/
+passport.serializeUser((user, cb) => {
+    cb(null, user.id);
 });
-//passport deserializedUser is going to take the id and look that up in the dattabase
-passport.deserializeUser((id, cb)=>{
-    cb(null, id)
-    .catch(cb());
-        
-    
-})
-passport.use(new localStategy({
-    usernameField: "email",
-    passwordField: "password"
-}, (email, password, cb)=>{
+// Passport "deserializeUser" is going to take the id and look that 
+// up in the database
+passport.deserializeUser((id, cb) => {
+    cb(null, user.id)
+    .catch(cb);
+    db.user.findByPk(id)
+    .then(user => {
+        cb(null, user)
+    }).catch(cb);
+});
+passport.use(new localStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, (email, password, cb) => {
     db.user.findOne({
-        where: {email}
+        where: { email }
     })
-    .then(user =>{
-        if(!user || !user.validPassword(password)) {
+    .then(user => {
+        if (!user || !user.validPassword(password)) {
             cb(null, false);
-        }else{
-            cb(null, user)
+        } else {
+            cb(null, user);
         }
     })
-    .catch(cb());
-}
-))
+    .catch(cb);
+}));
 module.exports = passport;
--- test/auth.test.js
+++ test/auth.test.js
@@ -3,6 +3,7 @@ var request = require('supertest');
 var app = require('../server');
 var db = require('../models');
 
+
 before(function(done) {
   db.sequelize.sync({ force: true }).then(function() {
     done();
