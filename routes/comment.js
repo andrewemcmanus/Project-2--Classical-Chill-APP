ec53807c57769b37ca98fa8211a8cffcb6bcb688
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-08-19 23:46:02 -0700

    work on css

--- routes/comment.js
+++ routes/comment.js
@@ -26,15 +26,16 @@ router.post('/:id', (req, res)=>{
 })
 
 router.put("/:id", (req, res)=>{
-    db.comment.update({
+    db.comment.update({// what do you want to updaye whrn you updat? which columns
         name: req.body.name,
         content: req.body.content,
         apiTrackId: req.params.id
     },{
 
-        where:{id: req.body.id}
+        where:{id: req.body.id}//open
     }).then(response=>{
         console.log(response)
+        res.redirect(`/track/${req.params.id}`)
     }).catch(err=>{
         console.log("error", err)
     })
