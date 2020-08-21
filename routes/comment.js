39b95f53c31fa3f073ea836dec0b6ad15e34e35b
Nicholas Phillips <nicholasphillips@Nicholass-MacBook-Air.local>
2020-08-21 16:40:05 -0700

    create delete route for comments

--- routes/comment.js
+++ routes/comment.js
@@ -17,7 +17,7 @@ router.post('/:id', (req, res)=>{
     })//create a comment to the body
     .then(addComment => {
         console.log(req.body)
-        res.redirect(`/track/${req.params.id}`)//got from the router above
+        res.redirect(`/track/${req.params.id}`)//gp to track?:id(string)
     })
     .catch(err=>{
         console.log('Error', err)
@@ -34,11 +34,39 @@ router.put("/:id", (req, res)=>{
 
         where:{id: req.body.id}//open
     }).then(response=>{
-        console.log(response)
+        // console.log(response)
+        console.log("put route for comment", req.params)
         res.redirect(`/track/${req.params.id}`)
     }).catch(err=>{
         console.log("error", err)
     })
+})
+router.delete("/:id", (req, res)=>{
+    console.log("delete route for comment", req.params)
+    console.log("result", req.body)
+    db.comment.destroy({
+        where: { id: req.params.id }
+      }).then(numRowsDeleted => {
+          console.log(numRowsDeleted)
+        // do something when done deleting
+        console.log('row deleted in table', req.params.id)
+        console.log('query inside of delete route', req.query)
+        
+        res.redirect(`/track/${req.body['track-id']}`);
+      });
+    // db.comment.destroy({//delete comment
+    //     name: req.body.name,
+    //     content: req.body.content,
+        
+    // },{
+
+    //     where:{id: req.body.id}//open
+    // }).then(response=>{
+    //     console.log(response)
+    //     res.redirect(`/track/${req.params.id}`)
+    // }).catch(err=>{
+    //     console.log("error", err)
+    // })
 })
 
 
